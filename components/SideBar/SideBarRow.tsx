0001bb1d8e86c8bce91bb96bd64d58b7b6bf591a
kushagra-aa <kushagraagnihotri24@gmail.com>
2022-05-21 22:06:40 +0530

    fixed sidebar props error

--- components/SideBar/SideBarRow.tsx
+++ components/SideBar/SideBarRow.tsx
@@ -3,7 +3,7 @@ import { SVGProps } from 'react'
 interface Props {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
   title: string
-  onClick?: () => {}
+  onClick?: any
 }
 
 const SideBarRow = ({ Icon, title, onClick }: Props) => {
